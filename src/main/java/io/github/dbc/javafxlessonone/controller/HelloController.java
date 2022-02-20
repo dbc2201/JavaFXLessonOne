1d5f943136c1bf356df92538be4f765c44e03914
dbc2201 <divyanshb22@gmail.com>
2022-02-21 01:49:49 +0530

    create methods to handle button clicks

--- src/main/java/io/github/dbc/javafxlessonone/controller/HelloController.java
+++ src/main/java/io/github/dbc/javafxlessonone/controller/HelloController.java
@@ -7,25 +7,59 @@ public class HelloController {
 
     @FXML
     public Button modalityNoneButton;
-
     @FXML
     public Button applicationModalButton;
-
     @FXML
     public Button windowModalButton;
-
     @FXML
     public Button stageStyleDecoratedButton;
-
     @FXML
     public Button stageStyleUndecoratedButton;
-
     @FXML
     public Button stageStyleTransparentButton;
-
     @FXML
     public Button stageStyleUtilityButton;
-
     @FXML
     public Button stageStyleUnifiedButton;
+
+    @FXML
+    public void onModalityNoneButtonClick() {
+
+    }
+
+    @FXML
+    public void onApplicationModalButtonClick() {
+
+    }
+
+    @FXML
+    public void onWindowModalButtonClick() {
+
+    }
+
+    @FXML
+    public void onStageStyleDecoratedButtonClick() {
+
+    }
+
+    @FXML
+    public void onStageStyleUndecoratedButtonClick() {
+
+    }
+
+    @FXML
+    public void onStageStyleTransparentButtonClick() {
+
+    }
+
+    @FXML
+    public void onStageStyleUtilityButtonClick() {
+
+    }
+
+    @FXML
+    public void onStageStyleUnifiedButtonClick() {
+
+    }
+
 }
