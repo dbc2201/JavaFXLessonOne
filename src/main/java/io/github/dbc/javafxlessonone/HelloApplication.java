2fd1a0a7dc7af2091487a5e1f4bcf5affec63fe5
dbc2201 <divyanshb22@gmail.com>
2022-02-21 01:37:29 +0530

    add button IDs in fxml, and add corresponding fields for buttons in controller

--- src/main/java/io/github/dbc/javafxlessonone/HelloApplication.java
+++ src/main/java/io/github/dbc/javafxlessonone/HelloApplication.java
@@ -17,6 +17,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),
+                // These Screen.getPrimary().getBounds().getMin methods will give you minimum width and height
                 Screen.getPrimary().getBounds().getMinX(),
                 Screen.getPrimary().getBounds().getMinY());
         stage.setTitle("JavaFX Lesson One");
--- src/main/java/io/github/dbc/javafxlessonone/controller/HelloController.java
+++ src/main/java/io/github/dbc/javafxlessonone/controller/HelloController.java
@@ -1,5 +1,31 @@
 package io.github.dbc.javafxlessonone.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class HelloController {
 
+    @FXML
+    public Button modalityNoneButton;
+
+    @FXML
+    public Button applicationModalButton;
+
+    @FXML
+    public Button windowModalButton;
+
+    @FXML
+    public Button stageStyleDecoratedButton;
+
+    @FXML
+    public Button stageStyleUndecoratedButton;
+
+    @FXML
+    public Button stageStyleTransparentButton;
+
+    @FXML
+    public Button stageStyleUtilityButton;
+
+    @FXML
+    public Button stageStyleUnifiedButton;
 }
